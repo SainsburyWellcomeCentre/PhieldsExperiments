74ec50cb0c6aeb866dedb4b1c9d1d3cb7129cc0c
nightswimmer <tarquinio@gmail.com>
2024-12-12 22:46:29 +0000

    Remove Bonsai32 after setup

--- .bonsai/setup.ps1
+++ .bonsai/setup.ps1
@@ -2,5 +2,6 @@ if (!(Test-Path "./Bonsai.exe")) {
     Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.9.0-preview1/Bonsai.zip" -OutFile "temp.zip"
     Expand-Archive "temp.zip" -DestinationPath "." -Force
     Remove-Item -Path "temp.zip"
+    Remove-Item -Path "Bonsai32.exe"
 }
 & .\Bonsai.exe --no-editor
