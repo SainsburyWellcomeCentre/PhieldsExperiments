4e152af0910a89b40f27d61a78698a12f4cace65
nightswimmer <tarquinio@gmail.com>
2024-12-13 12:26:56 +0000

    Add utility to remove \r \n from seral messages
    Co-authored-by: aspaNeuro <a.almeida@neurogears.org>

---
+++ src/bonsai/Extensions/RemoveEOL.cs
@@ -0,0 +1,22 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class RemoveEOL
+{
+    public IObservable<string> Process(IObservable<string> source)
+    {
+        return source.Select(value => 
+        {
+            string result = value.Replace("\n",String.Empty);
+            result = value.Replace("\r",String.Empty);
+            return result;
+        });
+    }
+}
