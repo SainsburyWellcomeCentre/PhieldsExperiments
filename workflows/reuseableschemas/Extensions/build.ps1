5d8a3e5abfcd15a79bcf452559688ccac2ad674b
NeuroThom <t.ryan@neurogears.org>
2025-07-20 17:09:47 +0100

    Add script and instructions to build classes

---
+++ workflows/reuseableschemas/Extensions/build.ps1
@@ -0,0 +1,7 @@
+dotnet bonsai.sgen --schema ../Schemas/UndergroundFeeder.json --serializer YamlDotNet --namespace Feeders
+dotnet bonsai.sgen --schema ../Schemas/CameraController.json --serializer YamlDotNet --namespace CameraController
+dotnet bonsai.sgen --schema ../Schemas/MechanicalLoom.json --serializer YamlDotNet --namespace Looms
+dotnet bonsai.sgen --schema ../Schemas/TimestampSynchronizer.json --serializer YamlDotNet --namespace TimestampSynchronizer
+dotnet bonsai.sgen --schema ../Schemas/Spinnaker.json --serializer YamlDotNet --namespace Cameras
+dotnet bonsai.sgen --schema ../Schemas/CameraController.json --serializer YamlDotNet --namespace CameraController
+dotnet bonsai.sgen --schema ../Schemas/RigConfiguration.json --serializer YamlDotNet --namespace RigConfiguration 
